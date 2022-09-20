9eb4a46a1bbc4633fc551fd914d7bb63efd55cb3
Shir_qs82 <Shir_qs82@STHQ0129-11>
2022-09-20 18:04:09 +0400

    last version

--- WordTaskWPF/MainWindow.xaml.cs
+++ WordTaskWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,41 @@ namespace WordTaskWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string helpString { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void cutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Cut();
+        }
+
+        private void copyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Copy();
+        }
+
+        private void PasteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Paste();
+        }
+
+        private void SelectAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.SelectAll();
+        }
+
+        private void infoItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("It is just a info");
+        }
+
+        private void SaveItem_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "C:/Users/Shir_qs82/Desktop";
+            File.WriteAllText(path,TextBox1.Text);
         }
     }
 }
